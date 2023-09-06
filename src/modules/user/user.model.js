76f2c413bf010c3a03186ef3e2d4f1a56a85b5e2
JON REY L. LUMAYAG <jaezer.razor@gmail.com>
2023-09-06 21:54:47 +0800

    Add user

--- src/modules/user/user.model.js
+++ src/modules/user/user.model.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
-
+import bcrypt from "bcrypt";
 const userSchema = mongoose.Schema({
   email: {
     type: String,
@@ -14,5 +14,16 @@ const userSchema = mongoose.Schema({
     required: [true, "{PATH} is required"],
   },
 });
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified()) return next();
+
+  this.password = await bcrypt.hashSync(this.password, 12);
+  return next();
+});
+
+userSchema.methods.comparePassword = async function (password) {
+  return await bcrypt.compare(password, this.password).catch((e) => false);
+};
 
 export const User = mongoose.model("User", userSchema);
--- src/modules/user/user.route.js
+++ src/modules/user/user.route.js
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import { controller } from "./user.controller.js";
+
 const router = Router();
 router
   .get("/", controller.Get)
